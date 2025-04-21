1a6efe6fe0880b06b47e28c891528d7c60e44c1e
karemthabet <karemsabit.com>
2025-04-21 17:40:04 +0200

    counter app using cubit

---
+++ lib/bussiness_logic/cubit/cubit/counter_state.dart
@@ -0,0 +1,16 @@
+part of 'counter_cubit.dart';
+
+sealed class CounterState extends Equatable {
+  final int counter;
+  const CounterState(this.counter);
+  @override
+  List<Object?> get props => [counter];
+}
+
+final class CounterInitial extends CounterState {
+  const CounterInitial() : super(0);
+}
+
+final class CounterUpdate extends CounterState {
+  const CounterUpdate(int newCounter) : super(5);
+}
